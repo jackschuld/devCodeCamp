4100974e708a3f314adb32ef8039a1408fca606a
unknown <schuldjack@gmail.com>
2022-12-15 15:27:23 -0600

    Updating Day 9 Lab 2 Task 2

--- Labs/Day 9 - Python/data_lab_II.py
+++ Labs/Day 9 - Python/data_lab_II.py
@@ -1,4 +1,4 @@
-from refactor_lab_II import destinations, restaurants, transportations, entertainments
+#(USE FOR TASK 1)from refactor_lab_II import destinations, restaurants, transportations, entertainments
 
 ### TASK 1: A List of Destination Dictionaries ###
 
@@ -10,14 +10,14 @@ def create_dict(location_string, restaurants_list, transportations_list, enterta
     return {
         'Location': location_string,
         'Restaurants': restaurants_list,
-        'Transportation': transportations_list,
-        'Entertainment': entertainments_list
+        'Transportations': transportations_list,
+        'Entertainments': entertainments_list
     }
 
 
-chicago = create_dict(destinations[0], restaurants, transportations, entertainments)
-milwaukee = create_dict(destinations[1], restaurants, transportations, entertainments)
-cleveland = create_dict(destinations[2], restaurants, transportations, entertainments)
+# chicago = create_dict(destinations[0], restaurants, transportations, entertainments)
+# milwaukee = create_dict(destinations[1], restaurants, transportations, entertainments)
+# cleveland = create_dict(destinations[2], restaurants, transportations, entertainments)
 
 
 # 2) Each dictionary must have the following keys:
@@ -25,27 +25,75 @@ cleveland = create_dict(destinations[2], restaurants, transportations, entertain
 # Restaurants (list)
 # Transportations (list)
 # Entertainments (list)
-print(chicago)
-print(milwaukee)
-print(cleveland)
+
+# print(chicago)
+# print(milwaukee)
+# print(cleveland)
 
 # 3) Using your original lists for restaurants, transportations and entertainments, populate each dictionary’s list to be unique and add some new options.
 chicago_restaurants = ['Lou Malnati\'s', 'Harry Caray\'s', 'Portillo\'s']
 chicago_transportations = ['The L', 'Train']
-chicago_entertainments = ['Bulls/Blackhawks/Bears/Cubs/Sox Game', 'Lollapalooza', 'The Second City']
+chicago_entertainments = ['Go to a Game (Bulls/Blackhawks/Bears/Cubs/Sox)', 'Lollapalooza', 'The Second City']
 
 milwaukee_restaurants = ['Points East Pub', 'Zocalo Food Park', 'Sobelman\'s', 'Guadalajara']
 milwaukee_transportations = ['The Hop', 'Walk']
-milwaukee_entertainments = ['Bucks/Brewers Game', 'Summer Fest', 'Brewery Tour']
+milwaukee_entertainments = ['Go to a Game (Bucks/Brewers)', 'Summer Fest', 'Brewery Tour']
 
 cleveland_restaurants = ['Margaritaville', 'Saucy Brew Works', 'TownHall']
 cleveland_transportations = ['Trolley', 'Walk']
-cleveland_entertainments = ['Rock & Roll Hall of Fame', 'Browns/Guardians/Cavaliers Game', 'West Side Market']
+cleveland_entertainments = ['Go to a Game (Browns/Guardians/Cavaliers)', 'Rock & Roll Hall of Fame', 'West Side Market']
 
-chicago = create_dict(destinations[0], chicago_restaurants + restaurants, chicago_transportations + transportations, chicago_entertainments + entertainments)
-milwaukee = create_dict(destinations[1], milwaukee_restaurants + restaurants, milwaukee_transportations + transportations, milwaukee_entertainments + entertainments)
-cleveland = create_dict(destinations[2], cleveland_restaurants + restaurants, cleveland_transportations + transportations, cleveland_entertainments + entertainments)
+# Uncomment for TASK 2:
+destinations = ['Chicago', 'Milwaukee', 'Cleveland']
+chicago = create_dict(destinations[0], chicago_restaurants, chicago_transportations, chicago_entertainments)
+milwaukee = create_dict(destinations[1], milwaukee_restaurants, milwaukee_transportations, milwaukee_entertainments)
+cleveland = create_dict(destinations[2], cleveland_restaurants, cleveland_transportations, cleveland_entertainments)
 
-print(chicago)
-print(milwaukee)
-print(cleveland)
+# print(chicago)
+# print(milwaukee)
+# print(cleveland)
+
+
+### TASK 2: Refactor The Random Generator ###
+# 1) Switch to your refactor.py file. 
+# 2) Refamiliarize yourself and identify which functions will need to be refactored in order to randomly generate trip options from the newly organized data. (You may have to create new code/functions for this!)
+# 3) Refactor your code to handle the new data format, without changing how the program runs in the terminal.
+#       a. The random selections for restaurants, transportations & entertainments must come from the nested lists of the randomly selected destination dictionary.
+# 4) Once your code has been refactored to utilize the data from data.py, remove the original four lists from refactor.py
+
+def gen_destination(destination_key):
+    if destination_key == destinations[0]:
+        return chicago
+    elif destination_key == destinations[1]:
+        return milwaukee
+    elif destination_key == destinations[2]:
+        return cleveland
+    
+
+
+### TASK 3: New Data ##
+# Management called and wants to include the climate with each destination.
+# Add a climate field to each destination dictionary that describes its climate (temperate, tropical, etc)
+# Refactor your code to include the climate alongside the destination name, wherever it’s printed in the terminal.
+
+
+### TASK 4: Accessing Dictionary Values with Variables ###
+# Create a new module for storing dictionary keys.
+# Create a variable for each key of a destination dictionary
+# Import the entire keys module into your refactored module (do not individually import each key variable from the module)
+# Use the key variables from that module to access the values from the destination dictionaries instead of hard-coded strings.
+
+
+### BONUS: Nested Data ###
+# We must now offer hotel options for each destination!
+# Add a list of hotel dictionaries to each destination dictionary (at least three hotels for each destination)
+# A hotel dictionary must contain the following fields:
+# Name
+# Phone
+# Location (dictionary with the following fields)
+# Address
+# City
+# State
+# Zip Code
+# Update the refactored code to include a randomly selected hotel (only by name)
+# Upon user confirmation of their trip, display the hotel address and phone number.
--- Labs/Day 9 - Python/refactor_lab_II.py
+++ Labs/Day 9 - Python/refactor_lab_II.py
@@ -1,54 +1,66 @@
 from random import choice
+from data_lab_II import gen_destination
 
 destinations = ['Chicago', 'Milwaukee', 'Cleveland']
-restaurants = ['Pizza', 'Italian', 'Sushi', 'Seafood', 'Burgers']
-transportations = ['Car', 'Uber/Lyft']
-entertainments = ['Tourist Site', 'Explore Downtown', 'Bars/Clubs']
-
+# restaurants = ['Pizza', 'Italian', 'Sushi', 'Seafood', 'Burgers']
+# transportations = ['Car', 'Uber/Lyft']
+# entertainments = ['Tourist Site', 'Explore Downtown', 'Bars/Clubs']
 
 def gen_lists():
+    destination = gen_object(choice(destinations))
+    restaurant = choice(destination['Restaurants'])
+    tranportation = choice(destination['Transportations'])
+    entertainment = choice(destination['Entertainments'])
+    destination = destination['Location']
 
-    destination = choice(destinations)
-    restaurant = choice(restaurants)
-    tranportation = choice(transportations)
-    entertainment = choice(entertainments)
 
     return f'''
-    Destination: {destination}
-    Restaurant: {restaurant}
-    Transportation: {tranportation}
-    Entertainment: {entertainment}
+    GENERATED TRIP:
+        Destination: {destination}
+        Restaurant: {restaurant}
+        Transportation: {tranportation}
+        Entertainment: {entertainment}
     '''
 
 
 def select():
     response = ''
-    while response != 'y':
+    while response.upper() != 'Y':
+        print()
         trip = gen_lists()
+        print('----------------------------------------------')
         print(trip)
         response = input('Do you accept the generated trip? (y/n): ')
-    return trip
+    return f'\n\n\n\n\nYour final generated trip is...\n{trip}\n\n'
+
+
+def gen_object(selected_destination):
+    for destination in destinations:
+        if destination == selected_destination:
+            return gen_destination(destination)
+    
 
 
 def play_again():
-    if input('\nWould you like to play again? (y/n): ') == 'y':
+    if input('\n\n\nWould you like to play again? (y/n): ').upper() == 'Y':
         start()
     else:
-        print("\nThanks for playing!\n")
+        print("\n\n\n\n\n\nThanks for playing!!!\n\n\n##### TRIP GENERATOR #####\n\n\n")
 
 
 def intro():
     print("\n\n### Day Trip Generator ###\n")
     print('There are 4 decisions that need to be made - The destination, restaurant, mode of transport, and entertainment...')
-    print('First, we will show you a selection. Next, you will approve or disapprove the selection...')
-    print('Once you approve a selection for all 4 decisions, we will display the completed trip in the terminal...\n\n')
+    print('First, we will show you a selection for the destination. Next, you will approve or disapprove the selection...')
+    print('After approving the destination, we will display a restaurant, mode of transport, and entertainment for you to approve...')
+    print('Once all is approved, we will display the completed trip in the terminal...\n\n')
     input(['PRESS ENTER TO START THE DAY TRIP GENERATOR'])
 
 
 def start():
     intro()
-    select()
+    print(select())
     play_again()
 
 
-# start()
+start()
