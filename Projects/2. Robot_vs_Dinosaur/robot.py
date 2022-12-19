cab90bfd42dcb1e20b371be4876e7b0eff6f274f
unknown <schuldjack@gmail.com>
2022-12-19 16:11:44 -0600

    Uploading completed bonus task 1

--- Projects/2. Robot_vs_Dinosaur/robot.py
+++ Projects/2. Robot_vs_Dinosaur/robot.py
@@ -7,13 +7,19 @@ class Robot:
     
     # Displays enemies health and has user click enter to have the enemy attacked. Method works the same on dinosaur.py
     def attack(self, dinosaur):
-        count = 1
         print('\nRobot\'s turn!\n')
         print(f'{dinosaur.name}\'s health: {dinosaur.health}')
-        for weapon in self.active_weapons:
-            print(str(count) + ') ' + weapon.name)
-            count += 1
-        selection = input(f'Enter the number of the attack you want {self.name} to use against {dinosaur.name}: ')
-        # TODO base case if selection is not 1-3
+        selection = self.select(dinosaur)
         dinosaur.health -= self.active_weapons[int(selection) - 1].attack_power
         print(f'{dinosaur.name}\'s health is now {dinosaur.health}\n')
+    
+
+    def select(self, dinosaur):
+        selection = '0'
+        while int(selection) < 1 or int(selection) > len(self.active_weapons):
+            count = 1
+            for weapon in self.active_weapons:
+                print(str(count) + ') ' + weapon.name)
+                count += 1
+            selection = input(f'Enter the number of the attack you want {self.name} to use against {dinosaur.name}: ')
+        return selection
