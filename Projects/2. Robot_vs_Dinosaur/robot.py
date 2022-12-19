c6078e65c725395df64f40587e18e7288b2f215a
unknown <schuldjack@gmail.com>
2022-12-19 16:48:28 -0600

    Added notes detailing selection process

--- Projects/2. Robot_vs_Dinosaur/robot.py
+++ Projects/2. Robot_vs_Dinosaur/robot.py
@@ -5,17 +5,21 @@ class Robot:
         self.health = health
         self.active_weapons = active_weapons
     
-    # Displays enemies health and has user click enter to have the enemy attacked. Method works the same on dinosaur.py
+    # Method works the same on dinosaur.py
     def attack(self, dinosaur):
         print('\nRobot\'s turn!\n')
+        # Displays enemies health
         print(f'{dinosaur.name}\'s health: {dinosaur.health}')
+        # Assign weapon selection from the select method
         selection = self.select(dinosaur)
+        # Apply selection to enemy's health
         dinosaur.health -= self.active_weapons[int(selection) - 1].attack_power
         print(f'{dinosaur.name}\'s health is now {dinosaur.health}\n')
     
 
     def select(self, dinosaur):
         selection = '0'
+        # Will loop until the player enters a valid selection (1-3)
         while int(selection) < 1 or int(selection) > len(self.active_weapons):
             count = 1
             for weapon in self.active_weapons:
