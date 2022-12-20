90fef4c2ffe344455ce3aa687907359eabdd7f32
unknown <schuldjack@gmail.com>
2022-12-20 09:52:43 -0600

    Adding main.py

--- Projects/2. Robot_vs_Dinosaur/battlefield.py
+++ Projects/2. Robot_vs_Dinosaur/battlefield.py
@@ -1,9 +1,5 @@
 import time
 from random import randint
-from robot import Robot
-from dinosaur import Dinosaur
-from weapon import Weapon
-
 
 class Battlefield:
     
@@ -80,11 +76,3 @@ class Battlefield:
         print('https://www.asciiart.eu/animals/reptiles/dinosaurs - art by Joan Stark')
         print('https://ascii.co.uk/art/terminator - artist unknown')
 
-
-
-
-# Instance game
-game1 = Battlefield(Robot('Arnold', randint(80, 126), [Weapon('Punch', randint(16, 26)), Weapon('Sawed-off Shotgun', randint(10, 36)), Weapon('Grenade', randint(5, 46))]), Dinosaur('Sue', randint(90, 131), randint(15, 31)))
-
-# Run game instance
-game1.run_game()
